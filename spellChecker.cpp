5356c4594e0a16801c6bc8cd2893e241f91d644d
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2023-12-02 21:41:35 -0300

    Add find and highlight all wrong words

--- spellChecker.cpp
+++ spellChecker.cpp
@@ -104,6 +104,16 @@ string highlight_words(string& text, string& word, string color_code) {
     return colored_text;
 }
 
+int end_word(const string& text){
+    for (int i = 0; i < text.length(); ++i) {
+        if (ispunct(text[i]) || isspace(text[i])) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 //overload para caso já tenha as occorencias
 //red = "\033[1;31m"
 //blue = "\033[1;34m"
@@ -114,10 +124,12 @@ string highlight_wrong_words(string& text, string color_code, set<int>& occurren
     string red_color = color_code;
     int offset = 0;
     for (auto x : occurrences) {
-        colored_text.insert(x + offset, red_color);
-        offset += red_color.length();
-        colored_text.insert(x + word.length() + offset, reset_color);
-        offset += reset_color.length();
+        if(!isalpha(text[x-1])){
+            colored_text.insert(x + offset, red_color);
+            offset += red_color.length();
+            colored_text.insert(x + end_word(text.substr(x)) + offset, reset_color);
+            offset += reset_color.length();
+        }
     }
     return colored_text;
 }
@@ -283,6 +295,14 @@ vector<int> showErros(vector<string>& text, Trie*& dictionary) {
             wrong_words.push_back(i);
     }
     return wrong_words;
+}
+
+//returns all position of wrong words in the text
+void find_all_errors(vector<int> &wrong_words_in_array, string &text, vector<string> &all_words_of_text, int index, set<int> &occurrences){
+    if(index >= wrong_words_in_array.size())
+        return;
+    KMP(text,all_words_of_text[wrong_words_in_array[index]], occurrences);
+    find_all_errors(wrong_words_in_array,text,all_words_of_text,index + 1, occurrences);
 }
 
 // string random_corrections(string &text,vector<string> &wrong_words, vector<pair<string, int>>& suggestions){
