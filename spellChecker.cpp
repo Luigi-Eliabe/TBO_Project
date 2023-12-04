ebd27e6bbebdfe1554f8383d1ac9bcdf56adc718
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2023-12-03 23:30:48 -0300

    Update highlight words functions

--- spellChecker.cpp
+++ spellChecker.cpp
@@ -103,42 +103,34 @@ int end_word(const string& text){
 string highlight_words(string& text, string& word, string color_code) {
     string colored_text = text;
     string reset_color = RESET;
-    string red_color = color_code;
+    string color = color_code;
     set<int> occurrences;
     KMP(text, word, occurrences);
     int offset = 0;
     for (auto x : occurrences) {
-        colored_text.insert(x + offset, red_color);
-        offset += red_color.length();
-        colored_text.insert(x + word.length() + offset, reset_color);
-        offset += reset_color.length();
-    }
-    return colored_text;
-}
-
-int end_word(const string& text){
-    for (int i = 0; i < text.length(); ++i) {
-        if (ispunct(text[i]) || isspace(text[i])) {
-            return i;
+        if(!isalpha(text[x-1])){
+            colored_text.insert(x + offset, color);
+            offset += color.length();
+            colored_text.insert(x + end_word(text.substr(x)) + offset, reset_color);
+            offset += reset_color.length();
         }
     }
-
-    return -1;
+    return colored_text;
 }
 
 //overload para caso já tenha as occorencias
 //red = "\033[1;31m"
 //blue = "\033[1;34m"
 //green = "\033[1;32m"
-string highlight_wrong_words(string& text, string color_code, set<int>& occurrences) {
+string highlight_wrong_words(const string& text, string color_code, set<int>& occurrences) {
     string colored_text = text;
     string reset_color = RESET;
-    string red_color = color_code;
+    string color = color_code;
     int offset = 0;
     for (auto x : occurrences) {
         if(!isalpha(text[x-1])){
-            colored_text.insert(x + offset, red_color);
-            offset += red_color.length();
+            colored_text.insert(x + offset, color);
+            offset += color.length();
             colored_text.insert(x + end_word(text.substr(x)) + offset, reset_color);
             offset += reset_color.length();
         }
