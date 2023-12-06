1d37b451b63797dc03cfdf13fe0cdbcaf4d06416
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2023-12-05 21:12:37 -0300

    Add necessary comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <bits/stdc++.h>
 
-#define RED "\033[1;31m"
-#define BLUE "\033[1;34m"
-#define GREEN "\033[1;32m"
-#define RESET "\033[0m"
-#define DICTIONARY_NAME "pt_BR.dic"
+#define RED "\033[1;31m" //code to RED color
+#define BLUE "\033[1;34m" //code to BLUE color
+#define GREEN "\033[1;32m" //code to GREEN color
+#define RESET "\033[0m" //code to RESET the color
+#define DICTIONARY_NAME "pt_BR.dic" //Name of the dictionary file to use
 
 using namespace std;
 
+//Struct Trie Node
 struct TrieNode {
     map<char, TrieNode*> children;
     bool isEndOfWord;
 
     TrieNode() : isEndOfWord(false) {}
+
+    ~TrieNode() {
+        for (auto& child : children) {
+            delete child.second;
+        }
+    }
 };
 
 struct Trie {
@@ -20,6 +27,11 @@ struct Trie {
 
     Trie() : root(new TrieNode()) {}
 
+    ~Trie() {
+        delete root;
+    }
+
+    //Insert trie node
     void insert(TrieNode* node, const string& word) {
         TrieNode* current = node;
 
@@ -37,6 +49,7 @@ struct Trie {
         insert(root, word);
     }
 
+    //Search for a node
     bool search(TrieNode* node, const string& word) {
         TrieNode* current = node;
 
@@ -55,6 +68,7 @@ struct Trie {
     }
 };
 
+//Calculate the Longest Prefix Suffix used in the KMP
 vector<int> compute_failure_function(const string& pattern) {
     int m = pattern.length();
     vector<int> lps(m, 0);
@@ -80,6 +94,7 @@ vector<int> compute_failure_function(const string& pattern) {
     return lps;
 }
 
+//Text = string to find patterns | pattern = pattern | matches = set that maps all pattern positions in string
 void KMP(const string& text, const string& pattern, set<int>& matches) {
     int text_length = text.length();
     int pattern_length = pattern.length();
@@ -108,6 +123,7 @@ void KMP(const string& text, const string& pattern, set<int>& matches) {
     }
 }
 
+//Auxiliar function to find the end of a word
 int end_word(const string& text) {
     for (int i = 0; i < text.length(); ++i) {
         if (ispunct(text[i]) || isspace(text[i])) {
@@ -118,15 +134,16 @@ int end_word(const string& text) {
     return -1;
 }
 
+//Function to highlight words searching them in the text using KMP
 string highlight_words(string& text, string& word, string color_code) {
     string colored_text = text;
     string reset_color = RESET;
     string color = color_code;
     set<int> occurrences;
     KMP(text, word, occurrences);
-    int offset = 0;
+    int offset = 0; //used to format colored word
     for (auto x : occurrences) {
-        if (!isalpha(text[x - 1])) {
+        if (!isalpha(text[x - 1])) { //Just to ensure that the word is in the beginning 
             colored_text.insert(x + offset, color);
             offset += color.length();
             colored_text.insert(x + end_word(text.substr(x)) + offset, reset_color);
@@ -136,13 +153,14 @@ string highlight_words(string& text, string& word, string color_code) {
     return colored_text;
 }
 
+//Function to highlight errors to search words that alrady have mapped
 string highlight_wrong_words(const string& text, string color_code, set<int>& occurrences) {
     string colored_text = text;
     string reset_color = RESET;
     string color = color_code;
-    int offset = 0;
+    int offset = 0; //used to calculate the distance to insert color
     for (auto x : occurrences) {
-        if (!isalpha(text[x - 1])) {
+        if (!isalpha(text[x - 1])) { //Just to ensure that the word is in the beginning
             colored_text.insert(x + offset, color);
             offset += color.length();
             colored_text.insert(x + end_word(text.substr(x)) + offset, reset_color);
@@ -152,6 +170,7 @@ string highlight_wrong_words(const string& text, string color_code, set<int>& oc
     return colored_text;
 }
 
+//Helper function to calculate distances between words, to see if the result is satisfier or not
 int levenshtein_distance(const string& s1, const string& s2) {
     int m = s1.length();
     int n = s2.length();
@@ -188,11 +207,20 @@ int levenshtein_distance(const string& s1, const string& s2) {
     return dp[m][n];
 }
 
+//DFS used to navigate through the Trie to find close matching word for the misspelled one
+//PARAMETERS
+//node = Trie dictionary
+//current_word = the word that is forming through the Trie
+//target_word = word that is used to be compared
+//index = used to compare char to char between the atual node of the Trie and the target_word
+//distance = distance in the current iteraction
+//max_distance = max distance allowed to navigate through the Trie
+//suggestions = set that contain the distance between 2 words using levenshtein and the suggest wright word
 void dfs(TrieNode* node, const string& current_word, const string& target_word, int index, int distance, int max_distance, set<pair<int, string>>& suggestions) {
-    if (distance > max_distance)
+    if (distance > max_distance) //Case base to stop DFS
         return;
     int edit_distance = levenshtein_distance(current_word, target_word);
-    if (node->isEndOfWord && distance <= max_distance && edit_distance < 4) {
+    if (node->isEndOfWord && distance <= max_distance && edit_distance < 4) { //If the word is in the range of misspelled chars and the distance between the two words is closed put it in the set of suggestions
         suggestions.insert(make_pair(edit_distance, current_word));
     }
 
@@ -205,17 +233,22 @@ void dfs(TrieNode* node, const string& current_word, const string& target_word, 
 
         int newDistance = distance + (currentChar == childChar ? 0 : 1);
 
-        if (newDistance <= max_distance) {
+        if (newDistance <= max_distance) { //verify if the distance is in the range allowed to continue searching
             dfs(nextNode, current_word + childChar, target_word, (index == target_word.length() - 1 ? 0 : index + 1), newDistance, max_distance, suggestions);
         }
     }
 }
 
-
+//function especified to call the DFS
+//word = word to find possible corrections
+//dicitonary = dictionary Trie
+//suggestions = set that contain the distance between 2 words using levenshtein and the suggest wright word
+//max_distance = max distance allowed to navigate through the Trie
 void generate_corrections(const string& word, Trie* dictionary, set<pair<int, string>>& suggestions, int max_distance) {
     dfs(dictionary->root, "", word, 0, 0, max_distance, suggestions);
 }
 
+//Read the dictorary to a file, and put each word in a vector
 vector<string> read_dictionary_from_file(const string& filename) {
     vector<string> words;
 
@@ -234,12 +267,19 @@ vector<string> read_dictionary_from_file(const string& filename) {
     return words;
 }
 
+//Cleans the punctation from the text
 void remove_accent(string& word) {
     if (word[word.length() - 1] == ',' || word[word.length() - 1] == '.') {
         word.resize(word.length() - 1);
     }
 }
 
+//Splits the string into many word in a vector
+//PARAMETERS
+//input = text string to dismount
+//words_in_text = array of words that will not have "," and "." to make the operations easier
+//words_in_text_w_accent = array of words to recreate the original text
+//delimiter = char to navigate through the string text
 void split_string(const string& input, vector<string>& words_in_text, vector<string>& words_in_text_w_accent, char delimiter) {
     istringstream iss(input);
     string token;
@@ -252,6 +292,12 @@ void split_string(const string& input, vector<string>& words_in_text, vector<str
 
 }
 
+//function to read files and create a string based on the text and 2 vectors of string: 1 vector containing all words of the text without "." and "," | 1 vector containing all words with the pontuations to rebuild the string with colored words
+//PARAMETERS
+//filename = text file 
+//words = array of words that will not have "," and "." to make the operations easier
+//words_w_accent = array of words to recreate the original text
+//full_text = string that will me created by the text file
 void read_text_file(string filename, vector<string>& words, vector<string>& words_w_accent, string& full_text) {
     ifstream file(filename);
     full_text = "";
@@ -269,19 +315,26 @@ void read_text_file(string filename, vector<string>& words, vector<string>& word
     file.close();
 }
 
-
+//Find and return all indexes of words not listed in the dictionary
 vector<int> show_erros(vector<string>& text, Trie*& dictionary) {
     vector<int> wrong_words;
     for (int i = 0; i < text.size(); i++) {
-        transform(text[i].begin(), text[i].end(), text[i].begin(), [](char c) {
-            return std::tolower(c);
-            });
+        transform(text[i].begin(), text[i].end(), text[i].begin(), [](char c) { //
+            return std::tolower(c);                                             // used to put the word interated in the for in the lowercase for search in the trie
+            });                                                                 //
         if (!dictionary->search(text[i]))
             wrong_words.push_back(i);
     }
     return wrong_words;
 }
 
+//Function that find all occurences of misspelleds words in the text using KMP anvoid
+//PARAMETERS
+//wrong_words_in_array = vector that map all positions of wrong words in the ARRAY of words that represents the text
+//text = the actual text string
+//all_words_of_text = array of all words in the text
+//index = auxiliar variable to stop the recursion
+//occurrences = set that map all misspelled words in the TEXT STRING
 void find_all_errors(vector<int>& wrong_words_in_array, const string& text, vector<string>& all_words_of_text, int index, set<int>& occurrences) {
     if (index >= wrong_words_in_array.size())
         return;
@@ -289,11 +342,13 @@ void find_all_errors(vector<int>& wrong_words_in_array, const string& text, vect
     find_all_errors(wrong_words_in_array, text, all_words_of_text, index + 1, occurrences);
 }
 
+//Helper function to remove colors in a string
 string remove_color_codes(const string& input) {
     regex color_regex("\x1B\\[[0-9;]*m");
     return regex_replace(input, color_regex, "");
 }
 
+//Function to add word in the dictionary file 
 void add_to_dictionary(string word, string dictionary) {
     ofstream dic_file(dictionary, ios::app);
     if (!dic_file.is_open()) {
@@ -303,6 +358,14 @@ void add_to_dictionary(string word, string dictionary) {
     dic_file << word << endl;
 }
 
+//Switch all matching words with  its counterpart in the misspelled string
+//PARAMETERS
+//suggestions = set with the suggested corrections
+//text_arr = array containig all words of the text
+//wrong_pos_arr = vector that map all misspelled words int the ARRAY of words
+//corrected_text = variable where the text fully corrected will be saved colored
+//white_corrected_text = variable where the text fully corrected will be saved but in white
+//dictionary = dictionary trie
 void auto_correct(vector<set<pair<int, string>>>& suggestions, vector<string>& text_arr, vector<int>& wrong_pos_arr, string& corrected_text, string& white_corrected_text, Trie* dictionary) {
     corrected_text = "";
     white_corrected_text = "";
@@ -311,6 +374,7 @@ void auto_correct(vector<set<pair<int, string>>>& suggestions, vector<string>& t
     for (auto x : wrong_pos_arr) {
         if (!(suggestions[ct].empty())) {
             auto firstElement = *(suggestions[ct].begin());
+            //verify if the word is in the end to just color the word, to not color the "," or "."
             text_arr[x] = isalpha(text_arr[x][text_arr[x].length() - 1]) ? (GREEN + firstElement.second + RESET) : text_arr[x].replace(0, text_arr[x].length() - 1, (GREEN + firstElement.second + RESET));
         }
         else {
@@ -322,6 +386,7 @@ void auto_correct(vector<set<pair<int, string>>>& suggestions, vector<string>& t
     string cor = GREEN;
     for (auto x : text_arr) {
         string plain_text = remove_color_codes(x);
+        //Extra verification to not input some " " in the begin of the text string
         white_corrected_text = white_corrected_text + (white_corrected_text != "" ? " " : "") + plain_text;
         corrected_text = corrected_text + (corrected_text != "" ? " " : "") + x;
     }
@@ -342,7 +407,13 @@ void auto_correct(vector<set<pair<int, string>>>& suggestions, vector<string>& t
     }
 }
 
-
+//Receive a misspelled word and find all matching existing word with it
+//PARAETERS
+//suggestions = set with the suggested corrections
+//wrong_words_text = set that map all misspelled words in the TEXT STRING
+//text = the text to be navigated
+//dictionary = dctionary Trie
+//max_distance = max_distance to be used in the DFS
 void show_suggetions(vector<set<pair<int, string>>>& suggestions, set<int>& wrong_words_text, string& text, Trie* dictionary, int max_distance) {
     string wrong_word;
     for (auto x : wrong_words_text) {
@@ -352,6 +423,14 @@ void show_suggetions(vector<set<pair<int, string>>>& suggestions, set<int>& wron
     }
 }
 
+
+//Function that do manual corrections in the text
+//PARAMETERS
+//text_arr = array containg all words in the text string
+//wrong_pos_arr = set that map all misspelled words in the ARRAY of words
+//corrected_text = variable where the text fully corrected will be saved colored
+//white_corrected_text = variable where the text fully corrected will be saved but in white
+//dictionary = dictionary Trie
 void manual_corrections(vector<string>& text_arr, vector<int>& wrong_pos_arr, string& corrected_text, string& white_corrected_text, Trie* dictionary) {
     corrected_text = "";
     white_corrected_text = "";
@@ -360,7 +439,7 @@ void manual_corrections(vector<string>& text_arr, vector<int>& wrong_pos_arr, st
         cout << "\nInput the correction to " << RED << (isalpha(text_arr[x][text_arr[x].length() - 1]) ? text_arr[x] : text_arr[x].substr(0, text_arr[x].size() - 1)) << RESET << ": ";
         cin >> correction;
         text_arr[x] = isalpha(text_arr[x][text_arr[x].length() - 1]) ? (GREEN + correction + RESET) : text_arr[x].replace(0, text_arr[x].length() - 1, (GREEN + correction + RESET));
-        if (!dictionary->search(correction)) {
+        if (!dictionary->search(correction)) { //Here the input of the corrected word to is called when the word ins't in the dictionary
             cout << "Word " << BLUE << correction << RESET << " not found in the dictionary, do you want do add it?\n\n1 - Yes.\n2 - No.\n";
             int choice;
             cin >> choice;
@@ -377,7 +456,7 @@ void manual_corrections(vector<string>& text_arr, vector<int>& wrong_pos_arr, st
     }
 }
 
-
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void print_header(const string& title, int& max_width) {
     max_width = max(max_width, static_cast<int>(title.length()) + 4);
     int width = max(static_cast<int>(title.length()) + 4, max_width);
@@ -391,11 +470,13 @@ void print_header(const string& title, int& max_width) {
     cout << '+' << string(width, '-') << '+' << endl;
 }
 
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void print_content(const string& description, int max_width) {
     int descWidth = max(static_cast<int>(description.length()) + 2, max_width);
     cout << '|' << left << setw(descWidth) << description << '|' << std::endl;
 }
 
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void menu() {
     int max_width = 0;
 
@@ -411,6 +492,7 @@ void menu() {
     print_header("", max_width);
 }
 
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void search_menu() {
     int max_width = 0;
 
@@ -426,6 +508,7 @@ void search_menu() {
     print_header("", max_width);
 }
 
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void suggest_correction_menu() {
     int max_width = 0;
 
@@ -441,6 +524,8 @@ void suggest_correction_menu() {
     print_header("", max_width);
 
 }
+
+//FUNCTION TO FORMAT PRINT THE TABLE IN THE TERMINAL MENU
 void suggest_save_file() {
 
     int max_width = 0;
@@ -471,14 +556,16 @@ int main() {
     string teste = "flores";
     int max_distance = 3;
 
+    cout << "Loading dictionary!" << endl;
+
     if (!dictionary_file.empty()) {
         cout << "Dictionary loaded successfully. Words read: " << dictionary_file.size() << endl;
+        for (const string& word : dictionary_file) {
+            dictionary->insert(word);
+        }
     }
     else {
         cout << "Empty dictionary or error reading the file." << endl;
-    }
-    for (const string& word : dictionary_file) {
-        dictionary->insert(word);
     }
 
     string inputFile;
@@ -486,7 +573,7 @@ int main() {
     cin >> inputFile;
     read_text_file(inputFile, words_in_text, words_in_text_w_accent, file_text);
 
-    system("clear");
+    [[maybe_unused]] int result = system("clear");
     for (int choice;;) {
         menu();
         cin >> choice;
@@ -498,7 +585,7 @@ int main() {
                 cout << "\n\nWhat word you trying to find? " << endl;
                 cin >> word_to_find;
                 string colored_find_text = highlight_words(file_text, word_to_find, BLUE);
-                system("clear");
+                [[maybe_unused]] int result = system("clear");
                 cout << colored_find_text << endl;
                 search_menu();
                 cin >> choice;
@@ -540,32 +627,34 @@ int main() {
                     break;
                 }
                 if (choice == 1) {
-                    show_suggetions(suggested_corrections, wrong_words_text, (after_corrections == "" ? file_text : after_corrections), dictionary, max_distance);
+                    show_suggetions(suggested_corrections, wrong_words_text, (after_corrections == "" ? file_text : after_corrections), dictionary, max_distance); //this function is called to find the wrong words in the text
                     auto_correct(suggested_corrections, words_in_text_w_accent, wrong_erros_pos_arr, wright_word, after_corrections, dictionary);
                     suggested_corrections.clear();
                     words_in_text_w_accent.clear();
                     wrong_erros_pos_arr.clear();
                     wrong_words_text.clear();
                     words_in_text.clear();
-                    split_string(after_corrections, words_in_text, words_in_text_w_accent, ' ');
-                }                
-else if (choice == 2) {
-                    show_suggetions(suggested_corrections, wrong_words_text, (after_corrections == "" ? file_text : after_corrections), dictionary, max_distance);
+                    split_string(after_corrections, words_in_text, words_in_text_w_accent, ' '); //generate the text string already corrected
+                }
+                else if (choice == 2) {
+                    show_suggetions(suggested_corrections, wrong_words_text, (after_corrections == "" ? file_text : after_corrections), dictionary, max_distance); //this function is called to find the wrong words in the text
                     manual_corrections(words_in_text_w_accent, wrong_erros_pos_arr, wright_word, after_corrections, dictionary);
                     suggested_corrections.clear();
                     words_in_text_w_accent.clear();
                     wrong_erros_pos_arr.clear();
                     wrong_words_text.clear();
                     words_in_text.clear();
-                    split_string(after_corrections, words_in_text, words_in_text_w_accent, ' ');
+                    split_string(after_corrections, words_in_text, words_in_text_w_accent, ' '); //generate the text string already corrected
                 }
-                file_text = after_corrections;
+                file_text = after_corrections; //rewrite the text string with corrections
             }
             break;
-        case 0: return 0;
+        case 0:
+            dictionary->~Trie(); //free memory
+            return 0;
         }
 
     }
-
+    dictionary->~Trie();
     return 0;
 }
